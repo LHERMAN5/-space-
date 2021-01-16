d2be6934df8a6d7d7edb25ee9f27bebd7376167b
LHERMAN5 <57547538+LHERMAN5@users.noreply.github.com>
2021-01-16 15:56:23 -0700

    DEFYING GRAVITY!!!!!
    Player floats right off of planet, but I'll fix it. This is fine.

--- Space_Sailor/src/Player.gd
+++ Space_Sailor/src/Player.gd
@@ -4,14 +4,32 @@ var speed : int = 100
 var jumpForce : int = 600
 var gravity : int = 800
 
-
+var grav : Vector2 = Vector2()
 
 var vel : Vector2 = Vector2()
 
+var pos : Vector2 = Vector2()
+
+var xdist: int = 0
+var ydist: int = 0
+
+var radius: int = 0
+
+var unit: int = 0
+
 onready var sprite : Sprite = get_node("Sprite")
 
+
 func _physics_process(delta):
-	vel.x = 0
+	pos = sprite.get_position()
+	
+	print(pos)
+	
+	pos.x = 232-pos.x
+	pos.y = 751-pos.y
+	
+	print(pos)
+	
 	
 	if Input.is_action_pressed("move_left"):
 		vel.x -= speed
@@ -20,11 +38,18 @@ func _physics_process(delta):
 		
 	vel = move_and_slide(vel, Vector2.UP)
 	
-	vel.y += gravity*delta
+	unit = sqrt(pos.x*pos.x+pos.y*pos.y)
 	
+	if pos.x*delta != 0:
+		vel.x += pos.x*delta*gravity/unit
+	if pos.y*delta != 0:
+		vel.y += pos.y*delta*gravity/unit
+	
+	#jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		vel.y -= jumpForce
 	
+	#flip sprite
 	if vel.x < 0:
 		sprite.flip_h = true
 	elif vel.x > 0:
