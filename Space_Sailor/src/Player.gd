4f039601eeba5a8fb2fee401cb9da21e526a1131
LHERMAN5 <57547538+LHERMAN5@users.noreply.github.com>
2021-01-17 01:16:21 -0700

    Press to YEET

--- Space_Sailor/src/Player.gd
+++ Space_Sailor/src/Player.gd
@@ -16,6 +16,8 @@ var planet = [Vector2(1430,966),Vector2(2660,475),Vector2(4430,1173),Vector2(578
 var standGrav = 10000000
 var specGrav = [.5,1,.6,1,.8]
 
+var start=false
+
 #onready var sprite : Sprite = get_node("Sprite")
 
 func getRad(position, planet):
@@ -25,9 +27,14 @@ func getAng(vec1,vec2):
 	return atan2(vec1.y-vec2.y,vec1.x-vec2.x)
 
 func _physics_process(delta):
+	
+	if Input.is_action_pressed("dive"):
+		start = true
+		
 	if Input.is_action_pressed("reset"):
 		position = Vector2(-1110.947,970.375)
 		vel = Vector2(0,0)
+		start = false
 		
 	accel = Vector2(0,0)
 	for i in range(planet.size()):
@@ -36,6 +43,10 @@ func _physics_process(delta):
 			gravity *= 10
 		accel.x += -gravity*cos(getAng(position,planet[i]))
 		accel.y += -gravity*sin(getAng(position,planet[i]))
+		
+	if !start:
+		accel = Vector2()
+		
 	vel.x += delta*accel.x
 	vel.y += delta*accel.y
 	
